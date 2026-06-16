070201eeeaa37c2084cac412a656d7913cb3cb1a
Vicloba <202224208_lopez@tesch.edu.mx>
2026-06-16 13:36:04 -0600

    Refactor EnvioCorreo model for primary key and names

--- correos/models.py
+++ correos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# 1. Modelo de Campañas
 class Campana(models.Model):
     nombre = models.CharField(max_length=200, verbose_name="Nombre de la campaña (Uso interno)")
     asunto = models.CharField(max_length=250, verbose_name="Asunto del correo", default="¡Boletín Informativo!")
@@ -12,14 +13,16 @@ class Campana(models.Model):
     def __str__(self):
         return self.nombre
 
+
+# 2. Modelo de Contactos (Limpio y automático)
 class EnvioCorreo(models.Model):
-    destinatario = models.EmailField(db_column='destinatario', primary_key=True, max_length=254)
+    # Usamos un ID autoincremental normal para evitar bloqueos de llaves primarias
+    id = models.AutoField(primary_key=True)
+    destinatario = models.EmailField(max_length=254, verbose_name="Correo del Destinatario")
 
     class Meta:
-        managed = False  
-        db_table = 'correos_enviocorreo'
-        verbose_name = "Correo Enviado"
-        verbose_name_plural = "Envíos de Correos"
+        verbose_name = "Correo Registrado"
+        verbose_name_plural = "Lista de Contactos"
 
     def __str__(self):
         return str(self.destinatario)
