47b5dd435b566a66fe1fe6e187d5ec13e6dc7003
Vicloba <202224208_lopez@tesch.edu.mx>
2026-06-16 10:43:43 -0600

    fix: activando registro de admin y c

--- config/settings.py
+++ config/settings.py
@@ -82,9 +82,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CONFIGURACIÓN DE SMTP PARA GMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587  
-EMAIL_USE_TLS = True  
-EMAIL_USE_SSL = False
+EMAIL_PORT = 456  
+EMAIL_USE_TLS = False  
+EMAIL_USE_SSL = True
 
 # Credenciales de envío seguro
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'vicky190486@gmail.com')
